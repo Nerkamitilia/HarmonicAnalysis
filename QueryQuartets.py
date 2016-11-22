8e6f4e50a7b953a2935964d8cb516e339a0c8683
Nestor Napoles <napulen@gmail.com>
2016-11-22 17:54:34 +0100

    Fixed query errors in the quartets fetched from MusicBrainz API

--- QueryQuartets.py
+++ QueryQuartets.py
@@ -18,9 +18,9 @@ mozart_mbid = 'b972f589-fb0e-474e-b64a-803b0364fa75'
 # MusicBrainz ID of Ludwig van Beethoven
 beethoven_mbid = '1f9df192-a621-4f54-8850-2c5373b7eac9'
 # Main dictionary
-string_quartets = {#haydn_mbid:{},
-                   mozart_mbid:{}}
-#                   beethoven_mbid:{}}
+string_quartets = {haydn_mbid:{},
+                   mozart_mbid:{},
+                   beethoven_mbid:{}}
 
 musicbrainzngs.set_useragent(
     "harmonic analysis of string quartets",
@@ -45,52 +45,68 @@ def filterQuartets(quartet_list, composer_id):
 def fillQuartetInformation(composed_quartets):
     quartet_count = len(composed_quartets)
     full_quartet_dict = dict()
-    for idx,quartet_id in enumerate(composed_quartets):
+    # Fetching information about each node
+    for idx,work_id in enumerate(composed_quartets):
         print '\tFetching quartet information...({}/{})'.format(idx+1,quartet_count)
-        quartet_info = musicbrainzngs.get_work_by_id(quartet_id, includes=['work-rels'])['work']
-        curr_quartet = {'title':quartet_info['title']}
-        curr_quartet['movement-list'] = {}
-        for related_work in quartet_info['work-relation-list']:
-            related_work_id = related_work['work']['id']
-            if related_work['type'] == 'parts':
-                # Detecting a parent opus
-                if 'direction' in related_work and related_work['direction'] == 'backward':
-                    parent_id = related_work['work']['id']
-                    parent_title = related_work['work']['title']
-                    if parent_id not in full_quartet_dict:
-                        full_quartet_dict[parent_id] = dict()
-                        full_quartet_dict[parent_id]['title'] = parent_title
-                        full_quartet_dict[parent_id]['work-list'] = {}
-                    full_quartet_dict[parent_id]['work-list'][quartet_id] = curr_quartet
-                # If not a parent opus, then it is a movement from the quartet
-                else:
-                    curr_quartet['movement-list'][related_work_id] = {'title':related_work['work']['title']}
+        work_info = musicbrainzngs.get_work_by_id(work_id, includes=['work-rels'])['work']
+        # Parts could have made an entry of this work already
+        if work_id in full_quartet_dict:
+            curr_node = full_quartet_dict[work_id]
+        # New work that has not been parsed before
+        else:
+            curr_node = {'title':work_info['title']}
+        # Filtering garbage mbid entries, like this: 30d4080a-d195-3f03-88e3-585aae505398
+        if 'work-relation-list' in work_info:
+            for related_work in work_info['work-relation-list']:
+                related_work_id = related_work['work']['id']
+                related_work_title = related_work['work']['title']
+                if related_work['type'] == 'parts':
+                    # Detecting a parent work for this node
+                    if 'direction' in related_work and related_work['direction'] == 'backward':
+                        parent_id = related_work_id
+                        parent_title = related_work['work']['title']
+                        curr_node['part-parent'] = parent_id
+                        if parent_id not in full_quartet_dict:
+                            # This child will instantiate the parent
+                            full_quartet_dict[parent_id] = dict()
+                            full_quartet_dict[parent_id]['title'] = parent_title
+                            full_quartet_dict[parent_id]['part-list'] = {}
+                        full_quartet_dict[parent_id]['part-list'][work_id] = curr_node
+                    else:
+                        if work_id not in full_quartet_dict:
+                            # This parent will instantiate itself in the dictionary
+                            full_quartet_dict[work_id] = curr_node
+                            curr_node['part-list'] = {}
+                        child_id = related_work_id
+                        child_title = related_work_title
+                        curr_node['part-list'][child_id] = {'title': child_title, 'part-parent':work_id}
+    # Filtering non-root works, e.g., (Op.53 No.1, Op.53 No.2, Op.53 No.3  ---> All contained within Op.53)
+    full_quartet_dict = { work_id: full_quartet_dict[work_id] for work_id in full_quartet_dict if 'part-parent' not in full_quartet_dict[work_id]}
     return full_quartet_dict
 
 def main(args):
-    for composer_id in string_quartets:
-        # Lucene query: Looking for all string quartets of the composers in the dictionary
-        composer_info = musicbrainzngs.get_artist_by_id(composer_id)['artist']
-        # Get the appropiate entry in the string_quartets dictionary
-        composer = string_quartets[composer_id]
-        # Fill the basic information of the composer
-        composer['name'] = composer_info['name']
-        composer['sort-name'] = composer_info['sort-name']
-        composer['quartet-list'] = {}
-        # Starting with quartet information. Make the query to the MusicBrainz API
-        query = 'type:Quartet AND arid:{}'.format(composer_id)
-        result = musicbrainzngs.search_works(query, limit=100)
-        if result['work-count'] > 100:
-            print ''' MORE WORKS LEFT, THIS NEEDS MORE QUERY ITERATIONS!! '''
-        quartet_list = result['work-list']
-        # Filter the list of "real" works from others, e.g., disputed-attributions, dedications, arrangements, etc.
-        composed_quartets, other_quartets = filterQuartets(quartet_list, composer_id)
-        # Complete all the information of the quartets, i.e., Opus and number, movements, etc.
-        print '{}:'.format(composer['name'])
-        full_quartet_dict = fillQuartetInformation(composed_quartets)
-        # Assign the quartet information to the composer entry in the dictionary
-        composer['quartet-list'] = full_quartet_dict
     with open(args.output_file, 'w') as outfile:
+        for composer_id in string_quartets:
+            # Lucene query: Looking for all string quartets of the composers in the dictionary
+            composer_info = musicbrainzngs.get_artist_by_id(composer_id)['artist']
+            # Get the appropiate entry in the string_quartets dictionary
+            composer = string_quartets[composer_id]
+            # Fill the basic information of the composer
+            composer['name'] = composer_info['name']
+            composer['sort-name'] = composer_info['sort-name']
+            # Starting with quartet information. Make the query to the MusicBrainz API
+            query = 'type:Quartet AND arid:{}'.format(composer_id)
+            result = musicbrainzngs.search_works(query, limit=100)
+            if result['work-count'] > 100:
+                print ''' MORE WORKS LEFT, THIS NEEDS MORE QUERY ITERATIONS!! '''
+            quartet_list = result['work-list']
+            # Filter the list of "real" works from others, e.g., disputed-attributions, dedications, arrangements, etc.
+            composed_quartets, other_quartets = filterQuartets(quartet_list, composer_id)
+            # Complete all the information of the quartets, i.e., Opus and number, movements, etc.
+            print '{}:'.format(composer['name'])
+            full_quartet_dict = fillQuartetInformation(composed_quartets)
+            # Assign the quartet information to the composer entry in the dictionary
+            composer['quartet-list'] = full_quartet_dict
         outfile.write(json.dumps(string_quartets))
 
 if __name__ == "__main__":
